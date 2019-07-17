3b568801b7f481358a68a373f4706ff66a2b8bbc
DeckerSU <support@decker.su>
2019-07-17 04:25:14 +0300

    KMD+AC bootstrap gen

---
+++ make_bootstrap.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+# KMD+AC bootstrap gen (c) Decker, 2019
+
+# --------------------------------------------------------------------------
+function init_colors() {
+    RESET="\033[0m"
+    BLACK="\033[30m"
+    RED="\033[31m"
+    GREEN="\033[32m"
+    YELLOW="\033[33m"
+    BLUE="\033[34m"
+    MAGENTA="\033[35m"
+    CYAN="\033[36m"
+    WHITE="\033[37m"
+    BRIGHT="\033[1m"
+    DARKGREY="\033[90m"
+}
+
+function log_print() {
+   datetime=$(date '+%Y-%m-%d %H:%M:%S')
+   echo -e [$datetime] $1
+}
+
+function bootstrap() {
+
+    if [ ! -z $1 ] && [ $1 != "KMD" ]
+        then
+            coin=$1
+            data_folder=${HOME}/.komodo/$coin
+            transform='s,^.,'${coin}',S'
+        else
+            coin="KMD"
+            data_folder=${HOME}/.komodo
+            transform=''
+    fi
+
+    archive_name=bootstrap.${coin,,}.tar.gz
+    du_files_list="${data_folder}/blocks ${data_folder}/chainstate ${data_folder}/notarisations" # ${data_folder}/komodostate ${data_folder}/komodostate.ind
+    tar_files_list="./blocks ./chainstate ./notarisations" #  ./komodostate ./komodostate.ind
+    files_size=$(du -cak ${du_files_list} | cut -f1 | tail -1)
+    checkpoint=$((files_size / 100))
+    log_print "Archiving \x5B${YELLOW}$i${RESET}\x5D --> ${archive_name}"
+    log_print "Directory: ${data_folder}"
+    log_print "Content: ${tar_files_list}"
+    log_print "Size: ${files_size} Kb, Checkpoint: ${checkpoint} Kb"
+
+    # --checkpoint-action=ttyout='[%{%Y-%m-%d %H:%M:%S}t] (%d sec): %u Kb, %T%*\r'
+    GZIP=-9 tar --directory ${data_folder} \
+        --record-size=1K --checkpoint="${checkpoint}" --checkpoint-action=ttyout='[%{%Y-%m-%d %H:%M:%S}t] Size: %u Kb, %T Elapsed: %d sec%*\r' \
+        --show-transformed-names --transform="${transform}" \
+        --exclude={wallet.dat,*.conf,*.bak,db.log,debug.log,fee_estimates.dat,peers.dat,banlist.dat} \
+        --exclude={backup_*.dat,.lock,komodod.pid} \
+        -czvf $(pwd)/${archive_name} ${tar_files_list} > /dev/null
+
+}
+
+# --- Variables ---
+#komodo_cli_binary="$HOME/komodo/src/komodo-cli"
+komodo_cli_binary="/home/decker/ssd_nvme/komodo_src_beta/src/komodo-cli"
+# -----------------
+#
+# https://www.gnu.org/software/bash/manual/html_node/Shell-Parameter-Expansion.html
+# https://stackoverflow.com/questions/2264428/how-to-convert-a-string-to-lower-case-in-bash
+# https://stackoverflow.com/questions/984204/shell-command-to-tar-directory-excluding-certain-files-folders
+# https://stackoverflow.com/questions/18681595/tar-a-directory-but-dont-store-full-absolute-paths-in-the-archive
+# https://www.gnu.org/software/tar/manual/html_section/tar_51.html#transform
+# https://unix.stackexchange.com/questions/72661/show-sum-of-file-sizes-in-directory-listing
+# https://www.gnu.org/software/tar/manual/html_section/tar_26.html#SEC48
+# -----------------
+
+init_colors
+echo "KMD+AC bootstrap gen (c) Decker, 2019"
+echo
+log_print "Start making bootstrap for KMD/AC ..."
+
+# https://stackoverflow.com/questions/18669756/bash-how-to-extract-data-from-a-column-in-csv-file-and-put-it-in-an-array
+
+# you can fill coins array from your local assetchains.json file
+# readarray -t kmd_coins < <(cat $HOME/komodo/src/assetchains.json | jq -r '[.[].ac_name] | join("\n")')
+# or directly from jl777/komodo beta branch assetchains.json
+readarray -t kmd_coins < <(curl -s https://raw.githubusercontent.com/jl777/komodo/beta/src/assetchains.json | jq -r '[.[].ac_name] | join("\n")')
+# you can spectify coins array manually if you want
+# declare -a kmd_coins=(BEER PIZZA KMD)
+# printf '%s\n' "${kmd_coins[@]}"
+for i in "${kmd_coins[@]}"
+do
+    bootstrap $i
+done
+
