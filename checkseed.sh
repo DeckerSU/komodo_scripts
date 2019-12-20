633f3860c45d8135142d5afe03c56c02c92d0c42
DeckerSU <support@decker.su>
2019-12-20 05:43:32 +0300

    simple dns seeder checker
    script can be used to check live nodes from DNS seeder in KMD
    network. it requests seeds from DNS and trying to connect each
    seed and request version message. example of work:
    
    Checking  139.99.121.200 - failed!
    Checking   149.56.19.212 - failed!
    Checking   159.65.134.48 - failed!
    Checking  178.128.25.203 - failed!
    Checking 217.182.203.106 - failed!
    Checking    52.65.58.103 - failed!
    Checking    66.70.180.46 - failed!
    Checking   78.47.196.146 - /MagicBean:3.0.0-beta1/
    Checking   88.99.251.101 - failed!
    Checking  95.213.205.222 - failed!
    Checking   95.213.238.98 - failed!

---
+++ checkseed.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+seeder=seeds.komodoplatform.com
+#seeder=static.kolo.supernet.org
+#seeder=dynamic.kolo.supernet.org
+
+readarray -t nodeips < <(nslookup ${seeder} | grep "Address: " | awk '{print $2}' | sed 's/:[0-9]*[0-9]*[0-9]*[0-9]*//g' | sort)
+#nodeips=(5.9.102.210 78.47.196.146 178.63.69.164 88.198.65.74 5.9.122.241 144.76.94.38 89.248.166.91)
+
+for ip in "${nodeips[@]}"
+do
+    # https://bitcointalk.org/index.php?topic=55852.0
+    # https://en.bitcoin.it/wiki/Protocol_documentation#Message_structure
+    # Newer protocol, # 18980200 - 170008
+    message='f9eee48d76657273696f6e0000000000640000005cee709118980200010000000000000011b2d05000000000010000000000000000000000000000000000ffff000000000000000000000000000000000000000000000000ffff0000000000003b2eb35d8ce617650f2f5361746f7368693a302e372e322fc03e0300'
+    #echo -n "Checking ${ip} - "
+    printf "Checking %15s - " ${ip}
+    echo ${message} | xxd -r -p | nc -w 3 -q 3 ${ip} 7770 > pattern.txt
+    if [ ! -s pattern.txt ]; then
+        echo "failed!"
+    else
+        len=$(cat pattern.txt | xxd -p -s 0x68 -l 1)
+        len=$((16#$len))
+
+        ua=$(cat pattern.txt | xxd -p -s 0x69 -l $len | xxd -r -p)
+        echo $ua
+    fi
+done
