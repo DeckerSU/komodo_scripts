c61528f366bc940fb457f22070991051600d960b
Anton "TonyL" Lysakov <tlysakov@gmail.com>
2020-06-22 06:01:08 +0700

    + Tecracoin (TCR)

--- genkomodo.php
+++ genkomodo.php
@@ -129,6 +129,7 @@ $coins = Array(
     Array("name" => "EMC2", "PUBKEY_ADDRESS" => 33, "SECRET_KEY" => 176),
     Array("name" => "GIN", "PUBKEY_ADDRESS" => 38, "SECRET_KEY" => 198),
     Array("name" => "AYA", "PUBKEY_ADDRESS" => 23, "SECRET_KEY" => 176),
+    Array("name" => "TCR", "PUBKEY_ADDRESS" => 65, "SECRET_KEY" => 58),
 );
 
 $k = hash("sha256", $passphrase);
