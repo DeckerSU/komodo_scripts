ff2ca9dfb79a90b0b230b6fdb63fcf3732a51286
DeckerSU <deckersu@protonmail.com>
2024-09-27 19:12:21 +0200

    add bech32 (native segwit) address calculation for BTC

--- genkomodo.php
+++ genkomodo.php
@@ -116,6 +116,87 @@ class BitcoinECDSADecker extends BitcoinECDSA {
     }
 }
 
+// bech32 related functions
+const BECH32_CHARSET = 'qpzry9x8gf2tvdw0s3jn54khce6mua7l';
+
+function bech32_polymod($values) {
+    $generator = [0x3b6a57b2, 0x26508e6d, 0x1ea119fa, 0x3d4233dd, 0x2a1462b3];
+    $chk = 1;
+    foreach ($values as $v) {
+        $top = $chk >> 25;
+        $chk = (($chk & 0x1ffffff) << 5) ^ $v;
+        for ($i = 0; $i < 5; $i++) {
+            if (($top >> $i) & 1) {
+                $chk ^= $generator[$i];
+            }
+        }
+    }
+    return $chk;
+}
+
+function bech32_hrp_expand($hrp) {
+    $expanded = [];
+    for ($i = 0; $i < strlen($hrp); $i++) {
+        $expanded[] = ord($hrp[$i]) >> 5;
+    }
+    $expanded[] = 0;
+    for ($i = 0; $i < strlen($hrp); $i++) {
+        $expanded[] = ord($hrp[$i]) & 31;
+    }
+    return $expanded;
+}
+
+function bech32_create_checksum($hrp, $data) {
+    $values = array_merge(bech32_hrp_expand($hrp), $data);
+    $values = array_merge($values, [0, 0, 0, 0, 0, 0]);
+    $polymod = bech32_polymod($values) ^ 1;
+    $checksum = [];
+    for ($i = 0; $i < 6; $i++) {
+        $checksum[] = ($polymod >> (5 * (5 - $i))) & 31;
+    }
+    return $checksum;
+}
+
+function bech32_encode($hrp, $data) {
+    $checksum = bech32_create_checksum($hrp, $data);
+    $combined = array_merge($data, $checksum);
+    $bech32 = $hrp . '1';
+    foreach ($combined as $p) {
+        $bech32 .= BECH32_CHARSET[$p];
+    }
+    return $bech32;
+}
+
+function convert_bits($data, $from_bits, $to_bits, $pad = true) {
+    $acc = 0;
+    $bits = 0;
+    $ret = [];
+    $maxv = (1 << $to_bits) - 1;
+    foreach ($data as $value) {
+        if ($value < 0 || ($value >> $from_bits)) {
+            return null;
+        }
+        $acc = ($acc << $from_bits) | $value;
+        $bits += $from_bits;
+        while ($bits >= $to_bits) {
+            $bits -= $to_bits;
+            $ret[] = ($acc >> $bits) & $maxv;
+        }
+    }
+    if ($pad) {
+        if ($bits > 0) {
+            $ret[] = ($acc << ($to_bits - $bits)) & $maxv;
+        }
+    } elseif ($bits >= $from_bits || (($acc << ($to_bits - $bits)) & $maxv)) {
+        return null;
+    }
+    return $ret;
+}
+
+function bytesArrayToHexString(array $bytes): string {
+    return implode('', array_map(fn($byte) => sprintf('%02x', $byte), $bytes));
+}
+
 $bitcoinECDSA = new BitcoinECDSADecker();
 
 $passphrase = "myverysecretandstrongpassphrase_noneabletobrute";
@@ -126,14 +207,14 @@ $coins = Array(
     Array("name" => "BTC",  "PUBKEY_ADDRESS" =>  0, "SECRET_KEY" => 128),
     Array("name" => "LTC",  "PUBKEY_ADDRESS" => 48, "SECRET_KEY" => 176),
     Array("name" => "KMD",  "PUBKEY_ADDRESS" => 60, "SECRET_KEY" => 188),
-    Array("name" => "GAME", "PUBKEY_ADDRESS" => 38, "SECRET_KEY" => 166),
-    Array("name" => "HUSH", "PUBKEY_ADDRESS" => Array(0x1C,0xB8), "SECRET_KEY" => 0x80),
+    // Array("name" => "GAME", "PUBKEY_ADDRESS" => 38, "SECRET_KEY" => 166),
+    // Array("name" => "HUSH", "PUBKEY_ADDRESS" => Array(0x1C,0xB8), "SECRET_KEY" => 0x80),
     Array("name" => "EMC2", "PUBKEY_ADDRESS" => 33, "SECRET_KEY" => 176),
-    Array("name" => "GIN", "PUBKEY_ADDRESS" => 38, "SECRET_KEY" => 198),
+    // Array("name" => "GIN", "PUBKEY_ADDRESS" => 38, "SECRET_KEY" => 198),
     Array("name" => "AYA", "PUBKEY_ADDRESS" => 23, "SECRET_KEY" => 176),
-    Array("name" => "GleecBTC", "PUBKEY_ADDRESS" => 35, "SECRET_KEY" => 65),
+    // Array("name" => "GleecBTC", "PUBKEY_ADDRESS" => 35, "SECRET_KEY" => 65),
     Array("name" => "MIL", "PUBKEY_ADDRESS" => 50, "SECRET_KEY" => 239),
-    Array("name" => "SFUSD", "PUBKEY_ADDRESS" => 63, "SECRET_KEY" => 188),
+    // Array("name" => "SFUSD", "PUBKEY_ADDRESS" => 63, "SECRET_KEY" => 188),
 );
 
 $k = hash("sha256", $passphrase);
@@ -152,27 +233,44 @@ echo PHP_EOL;
 
 foreach ($coins as $coin) {
 
-if (is_array($coin["PUBKEY_ADDRESS"])) {
-      $NetworkPrefix = bin2hex(implode("",array_map("chr", $coin["PUBKEY_ADDRESS"])));
-      $bitcoinECDSA->setNetworkPrefix($NetworkPrefix);
-} else
-      $bitcoinECDSA->setNetworkPrefix(sprintf("%02X", $coin["PUBKEY_ADDRESS"])); 
+    if (is_array($coin["PUBKEY_ADDRESS"])) {
+        $NetworkPrefix = bin2hex(implode("",array_map("chr", $coin["PUBKEY_ADDRESS"])));
+        $bitcoinECDSA->setNetworkPrefix($NetworkPrefix);
+    } else
+        $bitcoinECDSA->setNetworkPrefix(sprintf("%02X", $coin["PUBKEY_ADDRESS"]));
 
-// Returns the compressed public key. The compressed PubKey starts with 0x02 if it's y coordinate is even and 0x03 if it's odd, the next 32 bytes corresponds to the x coordinates.
-$NetworkPrefix = $bitcoinECDSA->getNetworkPrefix();
+    // Returns the compressed public key. The compressed PubKey starts with 0x02 if it's y coordinate is even and 0x03 if it's odd, the next 32 bytes corresponds to the x coordinates.
+    $NetworkPrefix = $bitcoinECDSA->getNetworkPrefix();
 
-echo "\x1B[01;37m[\x1B[01;32m "  . $coin["name"] . " \x1B[01;37m]\x1B[0m" . PHP_EOL;
-echo "         Network Prefix: " . $NetworkPrefix . PHP_EOL;
-echo "  Compressed Public Key: " . $bitcoinECDSA->getPubKey() . PHP_EOL;
-echo "Uncompressed Public Key: " . $bitcoinECDSA->getUncompressedPubKey() . PHP_EOL;
-echo "            Private Key: " . $bitcoinECDSA->getPrivateKey() . PHP_EOL;
-echo "         Compressed WIF: " . $bitcoinECDSA->getWIF( true, $coin["SECRET_KEY"]) . PHP_EOL;
-echo "       Uncompressed WIF: " . $bitcoinECDSA->getWIF(false, $coin["SECRET_KEY"]) . PHP_EOL;
+    echo "\x1B[01;37m[\x1B[01;32m "  . $coin["name"] . " \x1B[01;37m]\x1B[0m" . PHP_EOL;
+    echo "         Network Prefix: " . $NetworkPrefix . PHP_EOL;
+    echo "  Compressed Public Key: " . $bitcoinECDSA->getPubKey() . PHP_EOL;
+    echo "Uncompressed Public Key: " . $bitcoinECDSA->getUncompressedPubKey() . PHP_EOL;
+    echo "            Private Key: " . $bitcoinECDSA->getPrivateKey() . PHP_EOL;
+    echo "         Compressed WIF: " . $bitcoinECDSA->getWIF( true, $coin["SECRET_KEY"]) . PHP_EOL;
+    echo "       Uncompressed WIF: " . $bitcoinECDSA->getWIF(false, $coin["SECRET_KEY"]) . PHP_EOL;
 
-$address = $bitcoinECDSA->getAddress(); //compressed Bitcoin address
-echo "  Compressed Address: " . sprintf("%34s",$address) . PHP_EOL;
-$address = $bitcoinECDSA->getUncompressedAddress();
-echo "Uncompressed Address: " . sprintf("%34s",$address) . PHP_EOL;
+    $address = $bitcoinECDSA->getAddress(); //compressed Bitcoin address
+    echo "     Compressed Address: " . sprintf("%34s",$address) . PHP_EOL;
+    $address = $bitcoinECDSA->getUncompressedAddress();
+    echo "   Uncompressed Address: " . sprintf("%34s",$address) . PHP_EOL;
+    /* bech32 */
+    /*
+        - https://bitcointalk.org/index.php?topic=4992632
+        - https://en.bitcoin.it/wiki/Bech32
+        - https://en.bitcoin.it/wiki/BIP_0173
+        - https://www.reddit.com/r/Bitcoin/comments/62fydd/pieter_wuille_lecture_on_new_bech32_address_format/?rdt=49947
+        - https://learnmeabitcoin.com/technical/keys/#address-bech32
+    */
+    if ($coin["name"] === "BTC") {
+        $ripemd160 = $bitcoinECDSA->hash160(pack("H*",$bitcoinECDSA->getPubKey()));
+        // echo "             RIPEMD-160: " . $ripemd160 . PHP_EOL;
+        $data = convert_bits(array_values(unpack('C*', hex2bin($ripemd160))), 8, 5);
+        $hrp = 'bc'; $witness_version = 0;
+        $witness_data = array_merge([$witness_version], convert_bits(array_values(unpack('C*', hex2bin($ripemd160))), 8, 5));
+        $bech32_address = bech32_encode($hrp, $witness_data);
+        echo "Bech32 Address (P2WPKH): " . $bech32_address . PHP_EOL;
+    }
 }
 
 /* ETH/ERC20 */
