442cc43761ef49fefa240da81588972de8546c5b
DeckerSU <deckersu@protonmail.com>
2024-10-18 17:22:11 +0200

    add subsidy-3p.php

---
+++ subsidy-3p.php
@@ -0,0 +1,244 @@
+<?php
+
+$Notaries_elected_S8_3rdparty = Array(
+    "alright_DEV" => "02b698fee0945be7c2a489fc2ef93f956e491d5352228e5b4612219bba36590716",
+    "alien_AR" => "03ae0b483a41bafd5ac9820ef15ffe60091f795726fd0994dea4043dfa9863fb46",
+    "alien_EU" => "022b85908191788f409506ebcf96a892f3274f352864c3ed566c5a16de63953236",
+    "alien_NA" => "022f62b56ddfd07c9860921c701285ac39bb3ac8f6f083d1b59c8f4943be3de162",
+    "alien_SH" => "024f20c096b085308e21893383f44b4faf1cdedea9ad53cc7d7e7fbfa0c30c1e71",
+    "alienx_EU" => "025de0911bab55616c307f02ea8a5915a2e0c8e479aa97968e7f00d1025cbe6c6d",
+    "alienx_NA" => "025d5e11725233ab161f4f63d697c5f9f0c6b9d3aa2b9c68299638f8cc63faa9c2",
+    "artist_SH" => "03f0a59c15984d7627a38a7ee75fb4f4c41c6c6db58ff245efb429a1e7820c51dc",
+    "batman_EU" => "0373c4bf6cadb907597ef11d37d040cdb5dc73486beb0b8902837448f4087e0b7a",
+    "blackice_AR" => "032674b15524dab1c7a5824aa9d3d38f231a8a04095e11920677ee99d8197d9c60",
+    "blackice_EU" => "0271663454ffe07b7a13f25c93482bb554bab646627ee78941f6e59473a423e9c5",
+    "blackice_NA" => "035e356c96d4bc8ddd11109f679b44034fdd22003b87a8deeaae6ba2bb938f7e05",
+    "blackice_SH" => "0320b53f460e2867aacc578432d7ad5deb8f6f0da8e9c5ae65711011d692838bd4",
+    "blackice2_AR" => "022b10b7ec56b5c9f12bc9db1055edb3c2c0e0530453b7cc05239e4a3c442e80c4",
+    "ca333_EU" => "02da199f5da76cee5f09b4065e12ed454876308195cf45103e55f9837b4a1422d5",
+    "chmex_AR" => "030cd487e10fbf142e0e8d582e702ecb775f378569c3cb5acd0ff97b6b12803588",
+    "chmex_EU" => "030bf7bd7ad0515c33b5d5d9a91e0729baf801b9002f80495ae535ea1cebb352cb",
+    "chmex_NA" => "024e88a36d729352a391e07d1821dbfda1fca6409320cf9c2869b6fb99f05fbddd",
+    "chmex_SH" => "03e09c8ee6ae20cde64857d116c4bb5d50db6de2887ac39ea3ccf6434b1abf8698",
+    "chmexvet_SH" => "02d59db293de6c7da6673beeb373ebce62fd6d3522f715ea1356b5a2624fbd11a2",
+    "cipi_AR" => "02336758998f474659020e6887ece61ac7b8567f9b2d38724ebf77ae800c1fb2b7",
+    "cipi_EU" => "033a812d6cccdc4208378728f3a0e15db5b12074def9ab686ddc3752715ff1a194",
+    "cipi_NA" => "0335352862da521bd90b99d394db1ee3ecde379db9cf7ba2f28b16fa76153e289f",
+    "cipi2_NA" => "03b267592b0cb91d115524e7412e9ea79b30fbc8e29b8dcbbf850110f2e1d8c1ab",
+    "colmapol_EU" => "02754ae5585df0eba995f8e0aa1768b1424eaa04a3f1269a4cf37a8d4446f48a6d",
+    "dimxy_AR" => "0337e443df52f278f313f90628aaaa7a8db777f17bc7ce519069eb72717c1c2755",
+    "dragonhound_AR" => "039bb16266b0216264e7d3ccae12633105e1c14bd5d0e144e8b9c2b6d298a6c545",
+    "dragonhound_EU" => "0382d7e027bf5cda31264867f4e389a1a72f6671c7bd8818beb35b874d906e22d5",
+    "dragonhound_NA" => "02b4bf90b57f7c8363043c7ea8b02c8c776301ef3c1abb2df1caa2e6947be1422b",
+    "dragonhound_SH" => "02125e737f34c426474c9d29ec280bf49dc5ac99bffb6296b71a5031f58485ec86",
+    "fediakash_AR" => "035be6a54242a53e3ca55bd63430ac9b960fbfaad336d8c1464b5802f03ab184be",
+    "fediakash_NA" => "032485e92b4f06613c43123a178de96386e130311030958a736805d7b278df2f11",
+    "francois_SH" => "0285e1699d28cc6d134e476d85ee37badabc795f136df39e414fe6ca2b204e3a2f",
+    "gcharang_AR" => "039e01651c0afa1fc80b620301ff1981dd1db0f6c6b637b8f2f0fd986e9f5ece59",
+    "gcharang_SH" => "0321868e0eb39271330fa2c3a9f4e542275d9719f8b87773c5432448ab10d6943d",
+    "gcharang2_SH" => "0261b0337647329677b9885b050069a4737f139266c55ec711148af757a0276bc5",
+    "kmdude_SH" => "0253649e80366bb3a84c447ae2632d0ab272640be61ceb34291068c58531dae528",
+    "marmarachain_AR" => "0359809ec0774cf6a2679257c4db240b9061b08f72d9888e3323326f7428ddf93a",
+    "marmarachain_EU" => "02ca3e0618bc7c75afa6359ae476ee639682adfaa6fc463bbe7016c4f00da23ccf",
+    "mcrypt_NA" => "038e6f54c15eda9844e956206138494312648da158280a837341894fc642d8066a",
+    "naezith_EU" => "0310f58935bc72ed76f90301f8bcd8caea576603275493b9c664aa9ed25aa492c6",
+    "naezith_NA" => "031e48218c2e7fea3c852a3674848786c05d06fd451da4e14a6193363cf89febce",
+    "onur-ozkan_EU" => "02387f4093ba78ea6aa9023bd2b3e1d57a4d9d5e745a9cc6cb1bca2565c22f8c89",
+    "onur-ozkan_NA" => "03259f6aa4d3c451a52b7327bff4af9f649be3477459997b52711b54f4c8fb6394",
+    "pbca26_NA" => "025349e2ef906bb8dff17275d5d1034edf261d7b3b0fef02643c4e7227e161cd54",
+    "ptyx_SH" => "02eeaea84667462ae3e0c9262037666af81200b7982682ea06b690b1ac32a48c83",
+    "shamardy_SH" => "03589e28ad013c6ed3df0ed9947ecc2b6873a211be8aa3a745d3fea459b12a6435",
+    "smdmitry_AR" => "0397b7584cb29717b721c0c587d4462477efc1f36a56921f133c9d17b0cd7f278a",
+    "smdmitry2_AR" => "03c1efa0a64392e68cf50a13e4611b272b914cfba1e07f49f94389db3bac4497de",
+    "smdmitry_EU" => "0338f30ca34d0aca0d79b69abde447036aaaa75f482b6c75801fd382e984337d01",
+    "smdmitry_NA" => "038429f7abe33af2d6841029710b835223a05830fc8eee29521ec051e2f2d2e517",
+    "smdmitry_SH" => "03f7d5ac650baaccedab959adf7c4f416584f4c05a37bf079f710227560c456978",
+    "strob_SH" => "0213751a1c59d3489ca85b3d62a3d606dcef7f0428aa021c1978ea16fb38a2fad6",
+    "gcharang_DEV" => "03a3878af1152f648e6084fd3fbe697a26b1c2e92d407dd96c375f45f7d3ca13bf",
+    "dimxy_DEV" => "03a7edd6d0ba188960e39eced4d6b4ca6946bd98323ab40cbc13d6e52696de7dc4",
+    "ca333_DEV" => "0387b2a4c99127e871efeed28b73d6cf8296bb4aa8630e2cfb16490fdc47e0a561",
+    "qa_DEV" => "02ba8283b22c6572d83fd852e04928621dd0e2dd53150b5636ba64404ea73423bb",
+    "core_DEV" => "035509136135ba8e3f5d4733f7a9c160c2e1fefd8dc4658c3d95a5407e8da14749",
+    "security_DEV" => "037f666a67135f748ca3ffec29394662c71e917709bf61fee15a363655a8dff57e",
+    "tonyl_AR" => "03643e81eb0ac245c2caefe013b4357979f69ea585d6803182b230790dbcfb9ecf",
+    "van_EU" => "0370305b9e91d46331da202ae733d6050d01038ef6eceb2036ada394a48fae84b9",
+    "who-biz_AR" => "03a185b059983a5479e3adba44eb5dde8d899468632ba30480301a9f1bbbcbc4a8",
+    "who-biz_NA" => "026d5cecdee3021a80416c3d8ac68e59fd3d77a954bbe88a8eee794ade6c58f411",
+    "dragonhound_DEV" => "02a473e980bf0d198ece8ed11f1ecbe437edb688de6c83b82efa6f7de3a5d43c19"
+);
+
+// https://github.com/BitcoinPHP/BitcoinECDSA.php
+require_once 'BitcoinECDSA.php/src/BitcoinPHP/BitcoinECDSA/BitcoinECDSA.php';
+use BitcoinPHP\BitcoinECDSA\BitcoinECDSA;
+
+// bech32 related functions
+const BECH32_CHARSET = 'qpzry9x8gf2tvdw0s3jn54khce6mua7l';
+
+function bech32_polymod($values) {
+    $generator = [0x3b6a57b2, 0x26508e6d, 0x1ea119fa, 0x3d4233dd, 0x2a1462b3];
+    $chk = 1;
+    foreach ($values as $v) {
+        $top = $chk >> 25;
+        $chk = (($chk & 0x1ffffff) << 5) ^ $v;
+        for ($i = 0; $i < 5; $i++) {
+            if (($top >> $i) & 1) {
+                $chk ^= $generator[$i];
+            }
+        }
+    }
+    return $chk;
+}
+
+function bech32_hrp_expand($hrp) {
+    $expanded = [];
+    for ($i = 0; $i < strlen($hrp); $i++) {
+        $expanded[] = ord($hrp[$i]) >> 5;
+    }
+    $expanded[] = 0;
+    for ($i = 0; $i < strlen($hrp); $i++) {
+        $expanded[] = ord($hrp[$i]) & 31;
+    }
+    return $expanded;
+}
+
+function bech32_create_checksum($hrp, $data) {
+    $values = array_merge(bech32_hrp_expand($hrp), $data);
+    $values = array_merge($values, [0, 0, 0, 0, 0, 0]);
+    $polymod = bech32_polymod($values) ^ 1;
+    $checksum = [];
+    for ($i = 0; $i < 6; $i++) {
+        $checksum[] = ($polymod >> (5 * (5 - $i))) & 31;
+    }
+    return $checksum;
+}
+
+function bech32_encode($hrp, $data) {
+    $checksum = bech32_create_checksum($hrp, $data);
+    $combined = array_merge($data, $checksum);
+    $bech32 = $hrp . '1';
+    foreach ($combined as $p) {
+        $bech32 .= BECH32_CHARSET[$p];
+    }
+    return $bech32;
+}
+
+function convert_bits($data, $from_bits, $to_bits, $pad = true) {
+    $acc = 0;
+    $bits = 0;
+    $ret = [];
+    $maxv = (1 << $to_bits) - 1;
+    foreach ($data as $value) {
+        if ($value < 0 || ($value >> $from_bits)) {
+            return null;
+        }
+        $acc = ($acc << $from_bits) | $value;
+        $bits += $from_bits;
+        while ($bits >= $to_bits) {
+            $bits -= $to_bits;
+            $ret[] = ($acc >> $bits) & $maxv;
+        }
+    }
+    if ($pad) {
+        if ($bits > 0) {
+            $ret[] = ($acc << ($to_bits - $bits)) & $maxv;
+        }
+    } elseif ($bits >= $from_bits || (($acc << ($to_bits - $bits)) & $maxv)) {
+        return null;
+    }
+    return $ret;
+}
+
+function bytesArrayToHexString(array $bytes): string {
+    return implode('', array_map(fn($byte) => sprintf('%02x', $byte), $bytes));
+}
+
+class BitcoinECDSADecker extends BitcoinECDSA {
+
+    /***
+     * Tests if the address is valid or not.
+     *
+     * @param string $address (base58)
+     * @return bool
+     */
+    public function validateAddress($address)
+    {
+        $address    = hex2bin($this->base58_decode($address));
+
+        /*
+        if(strlen($address) !== 25)
+            return false;
+        $checksum   = substr($address, 21, 4);
+        $rawAddress = substr($address, 0, 21);
+	*/
+
+	$len = strlen($address);
+        $checksum   = substr($address, $len-4, 4);
+        $rawAddress = substr($address, 0, $len-4);
+
+        if(substr(hex2bin($this->hash256($rawAddress)), 0, 4) === $checksum)
+            return true;
+        else
+            return false;
+    }
+
+    /**
+     * Returns the current network prefix for WIF, '80' = main network, 'ef' = test network.
+     *
+     * @return string (hexa)
+     */
+    public function getPrivatePrefix($PrivatePrefix = 128){
+
+        if($this->networkPrefix =='6f')
+            return 'ef';
+        else
+           return sprintf("%02X",$PrivatePrefix);
+    }
+    /***
+     * returns the private key under the Wallet Import Format
+     *
+     * @return string (base58)
+     * @throws \Exception
+     */
+
+    public function getWIF($compressed = true, $PrivatePrefix = 128)
+    {
+        if(!isset($this->k))
+        {
+            throw new \Exception('No Private Key was defined');
+        }
+
+        $k          = $this->k;
+        
+        while(strlen($k) < 64)
+            $k = '0' . $k;
+        
+        $secretKey  =  $this->getPrivatePrefix($PrivatePrefix) . $k;
+        
+        if($compressed) {
+            $secretKey .= '01';
+        }
+        
+        $secretKey .= substr($this->hash256(hex2bin($secretKey)), 0, 8);
+
+        return $this->base58_encode($secretKey);
+    }
+}
+
+$bitcoinECDSA = new BitcoinECDSADecker();
+
+
+foreach ($Notaries_elected_S8_3rdparty as $key => $value) {
+    $bitcoinECDSA->setNetworkPrefix(sprintf("%02X", 0));
+    // $btc_address = $bitcoinECDSA->getUncompressedAddress(true, $value);
+    // echo $value . ' -> ' . $btc_address . PHP_EOL;
+
+    $pubkey = $bitcoinECDSA->getPubKey($bitcoinECDSA->getPubKeyPointsWithDerPubKey($value));
+
+    $ripemd160 = $bitcoinECDSA->hash160(pack("H*",$pubkey));
+    $data = convert_bits(array_values(unpack('C*', hex2bin($ripemd160))), 8, 5);
+    $hrp = 'bc'; $witness_version = 0;
+    $witness_data = array_merge([$witness_version], convert_bits(array_values(unpack('C*', hex2bin($ripemd160))), 8, 5));
+    $bech32_address = bech32_encode($hrp, $witness_data);
+    echo $key . ' -> ' . $bech32_address . PHP_EOL;
+}
+?>
